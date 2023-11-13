088744b640c3c4f44a0e4414e60d54099184f770
hugomsm <hugomaiadasilvamarques@gmail.com>
2023-11-13 14:34:00 -0300

    Update main.py

--- agenta-backend/agenta_backend/main.py
+++ agenta-backend/agenta_backend/main.py
@@ -25,10 +25,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 origins = [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://0.0.0.0:3000",
-    "http://0.0.0.0:3001",
+    "*"
 ]
 
 
